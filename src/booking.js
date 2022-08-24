bd00da10bcc0bde53a7db6b68a7178268ee01dc8
jpickard393 <jpickard@and.digital>
2022-08-24 16:07:03 +0100

    made tests pass green

--- src/booking.js
+++ src/booking.js
@@ -7,21 +7,33 @@ export default class Booking {
             this._bookedHours = [];
     };
 
-    addBooking(hourBooked){
-        this._bookedHours.push ({hourBooked: hourBooked});
+    addBooking(bookedHour){
+        if(this.validateBooking(bookedHour)){
+            this._bookedHours.push ({timeBooked: bookedHour});
+            return true;
+        }
+        return false;
     }
+
+    validateBooking(bookedHour){
+        return bookedHour !== undefined &&
+        this.isNotDoubleBooked(bookedHour) && 
+        this.isCorrectTimeFormat(bookedHour);
+    }
+
     // return a list of booked hours
     getBookedHours(){
-        console.log(this._bookedHours);
         return this._bookedHours;
     }
 
-    validateBooking(){
-        return false;
+    // check that there is not already a dublicate time in the array
+    isNotDoubleBooked(bookedHour){
+        return !this._bookedHours.includes(bookedHour);
     }
 
-    isCorrectTimeFormat(){
-        return false;
+    // check that the time is in format 00:00 and only on the hour
+    isCorrectTimeFormat(bookedHour){
+        const correctFormat = new RegExp("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$");
+        return correctFormat.test(bookedHour.timeBooked);
     }
-
 }
--- src/booking.test.js
+++ src/booking.test.js
@@ -1,20 +1,27 @@
 import Booking from "./booking";
 
+// SUT
+const booking = new Booking();
+
 describe ('Booking',()=>{
-    const booking = new Booking();
-    booking.addBooking('08:00');
+    // Arrange
+    const bookedHour = {timeBooked: '08:00'};
+
+    test('it SHOULD return true when a booking is added successfully',() => {
+        expect(booking.addBooking(bookedHour)).toBe(true);
+    });
 
     // Green
-    test('it SHOULD return a array of 1 bookedHours',() => {
+    test('it SHOULD return an array of 1 bookedHour',() => {
         expect(booking.getBookedHours()).toHaveLength(1);
     });
 
-    // no functionality yet.  Still red
-    test('it SHOULD return true if bookedHour is valid', () => {
-        expect (booking.validateBooking().toBeTruthy());
+    test('it SHOULD return False when bookedHour is not a duplicate', () => {
+        expect (booking.isNotDoubleBooked(bookedHour)).toBe(true);
     });
 
+    // Red
     test('it SHOULD return true if bookedHour is correct time format', () => {
-        expect (booking.isCorrectTimeFormat().toBeTruthy());
+        expect (booking.isCorrectTimeFormat(bookedHour)).toBe(true);
     });
 });
