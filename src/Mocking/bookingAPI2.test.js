18820daffca234fcb6904734be76bafde1f6cc10
jpickard393 <jpickard@and.digital>
2022-11-27 16:02:04 +0000

    improved booking factory can create multiple bookings

--- src/Mocking/bookingAPI2.test.js
+++ src/Mocking/bookingAPI2.test.js
@@ -58,12 +58,15 @@ describe ('bookingApi mocking experiments',() => {
     });
 
 
+    // booking factory - creates new rooms
     const createBooking = (roomId) => {
+        jest.resetModules();  // resets the cache of all required modules
         const bookingMock = jest.requireMock('../Booking');
-        const id = roomId;
+        const newRoomid = roomId;
+
         jest.mock('../Booking', () => (
             {
-                RoomId: id,
+                RoomId: newRoomid,
                 BookingDate: '01/10/2022',
                 EquipmentId: 1,
                 Capacity: 20
@@ -73,15 +76,22 @@ describe ('bookingApi mocking experiments',() => {
         return bookingMock;
     };
 
-    // test the factory
+    // test the factory method
     it.only('SHOULD return a new booking object with the correct roomId', () => {
-        const mockBooking = createBooking('My New Room');
+        // Arrange
+        const name1 = 'My New Room';
+        const name2 = 'My Second New Room';
+
+        const mockBooking = createBooking(name1);
+        const mockBooking2 = createBooking(name2);
         
         // Assert
-        expect(mockBooking.RoomId).toBe('My New Room');
+        expect(mockBooking.RoomId).toBe(name1);
+        expect(mockBooking2.RoomId).toBe(name2);
     }); 
 
     it('SHOULD return TRUE when an existing room is booked successfully', () => {
+        // Arrange
         bookingApi.addRoom(roomMock);
         const mockBooking = createBooking(roomMock.RoomId);
         
